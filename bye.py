c946cfbf8cf1b5c8b3697012eb4a82b0e7755246
VivasvanVerma <vvofficial.yt@gmail.com>
2025-06-07 15:30:30 +0530

    bye

--- bye.py
+++ bye.py
@@ -1,4 +1,8 @@
-n = int(input("Enter a number: "))
-if n%2 == 0:
-    while n == n:
-        print("bye")
+try:
+    n = int(input("Enter a number: "))
+    if n%2 == 0:
+        while n == n:
+            print("bye")
+
+except SyntaxError:
+    print("Only numbers allowed.")
